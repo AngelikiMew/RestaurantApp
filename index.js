b5c47bd30c5c4f31943cdd682850f8ce409558c5
AngelikiMew <angelikipapadaki@icloud.com>
2023-02-26 23:17:25 +0200

    yep it works

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-import {menuArray} from './data.js'
+//import {menuArray} from './data.js'
 
-
+const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        id: 2
+    }
+]
 
 
 
@@ -11,24 +33,20 @@ import {menuArray} from './data.js'
 
 function getContent(){
 
-    let createContent=``;
+    let createContent=""
+
     menuArray.forEach(function(menu){
+        console.log('here')
         createContent+=`
         <div class="menu-create">
             <div class='emoji'>
             <p>${menu.emoji}</p>
             </div>
-
-        
-        
-        
-        
-        
         </div>
         `
-        return createContent;
+        
     })
-
+    return createContent;
 }
 
 
