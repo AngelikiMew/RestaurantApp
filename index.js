8771bc7782ecea8170074a6df177aabb6a3cbb89
AngelikiMew <angelikipapadaki@icloud.com>
2023-02-26 22:48:45 +0200

    third

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+import {menuArray} from './data.js'
 
+
+
+
+
+
+
+
+
+
+function getContent(){
+   
+   return 'here'
+}
+
+
+
+
+function render(){
+    document.getElementById('content').innerHTML = getContent();
+}
+
+render();
